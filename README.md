5516c792509a349c63160757e95328bc5b8e31ad
Kevin Cox <kevincox@kevincox.ca>
2015-12-20 18:51:27 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backblaze b2 nix cache uploader.
+
+Script to build a nix expression and upload the resulting closure to backblaze b2 so that it can be used as a binary cache.
---
+++ upload.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+export LC_ALL=C
+
+bucket="$1"
+key="$2"
+
+cache=~/.cache/b2-nix-cache/exists-cache
+mkdir -p "$cache"
+
+store="$(mktemp -d)"
+trap 'rm -rf "$store"' EXIT
+
+nix-push --dest "$store" --key-file "$key" "$(nix-build)"
+
+comm -23 --check-order \
+	<(find "$store" -type f | cut -c$((${#store} + 2))- | sort) \
+	<(while :; do
+		r="$(backblaze-b2 list_file_names "$bucket" "$next" 1000)"
+		jq -r '.files[].fileName' <<<"$r"
+		
+		next="$(jq -r '.nextFileName' <<<"$r")"
+		[ "$(jq -r '.nextFileName | type' <<<"$r")" = 'null' ] && break
+	done) | \
+		parallel -j4 \
+			backblaze-b2 upload_file "$bucket" "$store/{}" "{}"
+
